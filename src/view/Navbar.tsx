c37bee17fd6c60c44c5c81b17d5655782061a992
David Marques <davidmarques@Davids-MacBook-Air.local>
2023-08-28 15:36:55 +0100

    navbar update

--- src/view/Navbar.tsx
+++ src/view/Navbar.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import logoImage from "../assets/team_logo.png";
 
 const NavBar: React.FC = () => {
+  function scrollToElement(id: string) {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
   return (
     <nav className="p-7 bg-gradient-to-b from-black to-transparent absolute top-0 w-full z-10">
       <div className="w-full">
@@ -11,17 +18,27 @@ const NavBar: React.FC = () => {
 
         <div className="flex">
           <div className="flex-grow w-[25%] text-center">
-            <a href="#about" className=" text-white hover:text-yellow-500">
+            <a
+              onClick={() => scrollToElement("about")}
+              className=" text-white hover:text-yellow-500 hover:cursor-pointer"
+            >
               Sobre
             </a>
           </div>
           <div className="flex-grow w-[25%] text-center">
-            <a href="#plantel" className=" text-white hover:text-yellow-500">
+            <a
+              onClick={() => scrollToElement("plantel")}
+              className=" text-white hover:text-yellow-500 hover:cursor-pointer"
+            >
               Plantel
             </a>
           </div>
           <div className="flex-grow w-[25%] text-center">
-            <a href="#contacts" className=" text-white hover:text-yellow-500">
+            <a
+              onClick={() => scrollToElement("contacts")}
+              href="#contacts"
+              className=" text-white hover:text-yellow-500 hover:cursor-pointer"
+            >
               Contatos
             </a>
           </div>
